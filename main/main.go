53585e3803f30e4345e45a0e35a8633b1655fe02
Dino Wernli <dinowernli@gmail.com>
2017-05-20 13:20:47 +0100

    Add a page for individual nodes.

--- main/main.go
+++ main/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 const (
-	nodesPath = "/nodes"
+	nodesPath       = "/nodes"
+	nodePathPattern = "/node/"
 )
 
 // TODO(dino): Split this yolo-file up into packages.
@@ -23,6 +25,7 @@ func main() {
 	// Create a handler and serve.
 	handler := &httpHandler{index: index}
 	http.HandleFunc(nodesPath, handler.nodes)
+	http.HandleFunc(nodePathPattern, handler.node)
 	http.ListenAndServe(":8080", nil)
 }
 
@@ -33,7 +36,16 @@ type httpHandler struct {
 }
 
 func (h *httpHandler) nodes(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Nodes: %+v", h.index.List())
+	fmt.Fprintf(w, "Hello world. Nodes: \n")
+	names := h.index.List()
+	for _, name := range names {
+		fmt.Fprintf(w, "<a href=\"%s%s\">%s</a>\n", nodePathPattern, name, name)
+	}
+}
+
+func (h *httpHandler) node(w http.ResponseWriter, r *http.Request) {
+	nodeName := strings.TrimPrefix(r.URL.Path, nodePathPattern)
+	fmt.Fprintf(w, "Hello world. Node name: %s", nodeName)
 }
 
 // storage
@@ -44,20 +56,33 @@ type graphStore struct {
 // indexing
 
 func createIndex(graphStore *graphStore) *graphIndex {
-	return &graphIndex{}
+	return &graphIndex{
+		nodes: []*nodeInfo{
+			&nodeInfo{name: "name1", message: "m1"},
+			&nodeInfo{name: "name2", message: "m2"},
+		},
+	}
 }
 
 type graphIndex struct {
+	nodes []*nodeInfo
 }
 
 type nodeInfo struct {
+	name    string
 	message string
 }
 
+// List returns the list of node names available in this index.
 func (i *graphIndex) List() []string {
-	return []string{"node1", "node2"}
+	result := []string{}
+	for _, node := range i.nodes {
+		result = append(result, node.name)
+	}
+	return result
 }
 
+// Get return the available information about the supplied node.
 func (i *graphIndex) Get(vname string) (*nodeInfo, error) {
 	return &nodeInfo{
 		message: "some node",
